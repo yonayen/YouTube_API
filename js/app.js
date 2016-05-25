20e8294df3ee8180ac16525cb1c5b0e56ee36f1d
Yonathan Ayenew <yonayen@gmail.com>
2016-05-25 07:22:27 -0700

    get request in JSON format

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 // --- API KEY: AIzaSyCKNNUXyaTDWJp14VhcwraU-mY7o4A27HY --- //
 
 // --- The endpoint: "https://www.googleapis.com/youtube/v3/search" --- //
+
+$(function() {
+	// two parameters: URL + function that will be called
+	$.getJSON('https://www.youtube.com/results?search_query=soccer', function(data){
+		console.log(data);
+	});
+});
