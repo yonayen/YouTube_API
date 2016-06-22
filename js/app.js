432464ad4413f4681c83da0fa6f393e245297b2b
Yonathan Ayenew <yonayen@gmail.com>
2016-06-22 16:57:12 -0700

    add tubeVideo as replacement (new instance of Video object) in html rendering

--- js/app.js
+++ js/app.js
@@ -1,19 +1,18 @@
+// Global Variables
+var tubeKey = 'AIzaSyCKNNUXyaTDWJp14VhcwraU-mY7o4A27HY';
+var tubeUrl = 'https://www.googleapis.com/youtube/v3/search';
+
 // Video object 
 var Video = function(tubeKey, tubeUrl, thumbnail, title, publishedAt, channelTitle)
 {
-	this.tubeKey = 'AIzaSyCKNNUXyaTDWJp14VhcwraU-mY7o4A27HY';
-	this.tubeUrl = 'https://www.googleapis.com/youtube/v3/search';
-	this.thumbnail = thumbnails.medium.url;
+	this.videoId =  videoId;
+	this.thumbnail = thumbnails;
 	this.title = title;
 	this.publishedAt = publishedAt;
 	this.channelTitle = channelTitle;
 }
 
-/*
-// Global Variables
-var tubeKey = 'AIzaSyCKNNUXyaTDWJp14VhcwraU-mY7o4A27HY';
-var tubeUrl = 'https://www.googleapis.com/youtube/v3/search';
-*/
+
 
 // --- Function takes query term, displays it and calls getRequest function with query -- //
 $(function() 
@@ -46,14 +45,19 @@ function showResults(results)
 	$.each(results, function(index, arrayitem){
 		if (counter < 9) {
 			counter ++;
+
+			var tubeVideo = new Video(arrayitem.id.videoId, arrayitem.snippet.thumbnails.medium.url, arrayitem.snippet.title, arrayitem.snippet.publishedAt, arrayitem.snippet.channelTitle);
+
 			html += '<div class="row">' + '<div class="post">' +
-				'<a href="https://www.youtube.com/watch?v='+ arrayitem.id.videoId + ' "><img src="' + arrayitem.snippet.thumbnails.medium.url + '"/></a>' +
-				'<h3 class="title">' + arrayitem.snippet.title + '</h3>' +
-				'<p class="post_info">' + arrayitem.snippet.publishedAt + ' | ' + arrayitem.snippet.channelTitle + '</p>'+ 
+				'<a href="https://www.youtube.com/watch?v='+ tubeVideo.videoId + ' "><img src="' + tubeVideo.thumbnails + '"/></a>' +
+				'<h3 class="title">' + tubeVideo.title + '</h3>' +
+				'<p class="post_info">' + tubeVideo.publishedAt + ' | ' + tubeVideo.channelTitle + '</p>'+ 
 				'</div>' +
           '</div>';
 		}
-	 console.log(arrayitem.snippet.title);
+
+	 console.log(tubeVideo.title);
+
 	});
 
 	$('#top_movies .wrapper').append(html);
