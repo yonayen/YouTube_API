c14ed33bd3d32dbee5b6b2cbb1245ed06c10b910
Yonathan Ayenew <yonayen@gmail.com>
2016-06-22 19:19:44 -0700

    remove unnecessary punctuation from comments

--- js/app.js
+++ js/app.js
@@ -2,19 +2,19 @@
 var tubeKey = 'AIzaSyCKNNUXyaTDWJp14VhcwraU-mY7o4A27HY';
 var tubeUrl = 'https://www.googleapis.com/youtube/v3/search';
 
-// --- Function takes query term, displays it and calls getRequest function with query -- //
+// Function takes query term, displays it and calls getRequest function with query
 $(function() 
 {
 	$('#search-button').on("click", function(event)
 	{
 		var searchTerm = $('#query').val();
-		// -- Takes search term and renders it on top of where clips are displayed -- //
+		// Takes search term and renders it on top of where clips are displayed
 		$('#top_movies .clearfix h2').text(searchTerm);
 		getRequest(searchTerm);
 	});
 });
 
-// -- Function sends request w/ query to API -- //
+// Function sends request w/ query to API
 function getRequest(searchTerm)
 {
 	$.getJSON(tubeUrl + '?part=snippet&key=' + tubeKey + '&q=' + searchTerm, function(data){
@@ -23,7 +23,7 @@ function getRequest(searchTerm)
 	});
 }
 
-// -- Function to display search results on page -- //
+// Function to display search results on page
 function showResults(results)
 {
 	$('#top_movies .wrapper .row').remove();
